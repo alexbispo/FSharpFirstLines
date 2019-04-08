5669de6604ecf61091b1f0ad73420fa0e76ab34c
alexbispo <alexbispo.artes@gmail.com>
2019-04-07 23:15:52 -0300

    My fisrt lines of code in F#.

---
+++ FirstApp/PeriodoDeTempo.fs
@@ -0,0 +1,16 @@
+﻿namespace FirstApp
+
+module PeriodoDeTempo =
+
+    open System
+
+    type Periodo = { DataInicial:DateTime; DataFinal:DateTime }
+
+    let adicionarDias periodo dias = {
+        DataInicial = periodo.DataInicial.AddDays dias
+        DataFinal = periodo.DataFinal.AddDays dias
+    }
+
+    let verificaSeDataEstaDentroDoPeriodo periodo (dataParaTestar:DateTime) = 
+        dataParaTestar.CompareTo periodo.DataInicial >= 0 && dataParaTestar.CompareTo periodo.DataFinal <= 0
+
---
+++ FirstApp/Program.fs
@@ -0,0 +1,37 @@
+﻿namespace FirstApp
+
+
+module Program =
+    open System
+    open System
+
+    let ``say hello`` =
+        printfn "Olá F#!"
+
+    let elevaNumeroAoQuadrado numero = numero * numero
+
+    let somaQuadradoDosNumerosAteDez =
+       [1..10] 
+       |> List.map elevaNumeroAoQuadrado 
+       |> List.sum
+
+    [<EntryPoint>]
+    let main argv =
+        ``say hello``
+
+        printfn "%i" somaQuadradoDosNumerosAteDez
+
+        let periodoTeste = { PeriodoDeTempo.DataInicial =  DateTime.Parse "21/11/1988"; PeriodoDeTempo.DataFinal = DateTime.Parse("29/10/1989") }
+
+        let datasParaTeste =
+            [|
+                DateTime.Parse "13/01/1989"
+                DateTime.Parse "05/05/1998"
+                DateTime.Parse "29/10/1989"
+            |]
+
+        Array.iter (fun e -> printfn "%b" (PeriodoDeTempo.verificaSeDataEstaDentroDoPeriodo periodoTeste e)) datasParaTeste
+    
+        Console.ReadKey() |> ignore
+
+        0 // return an integer exit code
